5b333124a10066e29e41455fb0b3c935e10b46b2
Yash <31120678+yash-js@users.noreply.github.com>
2021-06-25 11:01:49 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,7 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD29skcEC99IBqSJ6weBD_RaI8x7hOMRNs",
-  authDomain: "netflix-clone-ac04b.firebaseapp.com",
-  projectId: "netflix-clone-ac04b",
-  storageBucket: "netflix-clone-ac04b.appspot.com",
-  messagingSenderId: "86364470299",
-  appId: "1:86364470299:web:d5765a1c97e87df0964cbe",
-  measurementId: "G-47STD27MDK"
+// GET CONFIG FROM : https://console.firebase.google.com/
 };
 
 
